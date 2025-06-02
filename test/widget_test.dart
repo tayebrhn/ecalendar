cf0274731437354b9f272f9955f4cd779d04c50c
Taye Birhanu <tayyebirhan@gmail.com>
2025-06-02 13:00:41 +0300

    change icon

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:eccalendar/main.dart';
+import 'package:ecalendar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
